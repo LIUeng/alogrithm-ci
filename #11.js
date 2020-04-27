e397192664580eaef420030863c27553cf474d2a
LIUeng <386606235@qq.com>
2020-04-27 09:39:47 +0800

    :basketball: binary search variant

--- #11.js
+++ #11.js
@@ -37,3 +37,76 @@ function binarySearchRecursive(arr, low, high, value) {
 
 // result
 console.log('二分查找递归实现结果为：', binarySearchRecursive([1, 3, 4, 5, 6, 7, 8, 9, 9], 0, 9, 5));
+
+// 二分查找变形问题
+// 1. 查找第一个值等于给定值
+function binarySearchVariant1(arr, n, value) {
+    let low = 0, high = n - 1;
+    let mid = low + ((high - low) >> 1);
+    while(low <= high) {
+        if(arr[mid] == value) {
+            if(mid == 0 || arr[mid - 1] != value) {
+                return mid;
+            } else {
+                high = mid - 1;
+            }
+        } else if(arr[mid] > value) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+}
+
+// 2. 查找最后一个值等于给定值
+function binarySearchVariant2(arr, n, value) {
+    let low = 0, high = n - 1;
+    let mid = low + ((high - low) >> 1);
+    while(low <= high) {
+        if(arr[mid] == value) {
+            if(mid == 0 || arr[mid + 1] != value) {
+                return mid;
+            } else {
+                low = mid + 1;
+            }
+        } else if(arr[mid] > value) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+}
+
+// 3. 查找第一个大于等于给定值
+function binarySearchVariant3(arr, n, value) {
+    let low = 0, high = n - 1;
+    let mid = low + ((high - low) >> 1);
+    while(low <= high) {
+        if(arr[mid] >= value) {
+            if(mid == 0 || arr[mid - 1] < value) {
+                return mid;
+            } else {
+                high = mid - 1;
+            }
+        } else {
+            low = mid + 1;
+        }
+    }
+}
+
+// 4. 查找最后一个小于等于给定值
+function binarySearchVariant3(arr, n, value) {
+    let low = 0, high = n - 1;
+    let mid = low + ((high - low) >> 1);
+    while(low <= high) {
+        if(arr[mid] <= value) {
+            if(mid == n - 1 || arr[mid + 1] > value) {
+                return mid;
+            } else {
+                low = mid + 1;
+            }
+        } else {
+            high = mid - 1;
+        }
+    }
+}
