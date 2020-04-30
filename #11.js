9bd0b6105bfc7e72e0c55afa9bd65308cee86251
LIUeng <386606235@qq.com>
2020-05-01 00:04:33 +0800

    :football: update hashtable & binary tree

--- #11.js
+++ #11.js
@@ -5,12 +5,13 @@
 
 // 非递归实现
 function binarySearch(arr, n, value) {
-    let low = 0, high = n - 1;
-    while(low <= high) {
+    let low = 0,
+        high = n - 1;
+    while (low <= high) {
         let mid = (low + high) >> 1;
-        if(arr[mid] == value) {
+        if (arr[mid] == value) {
             return mid;
-        } else if(arr[mid] > value) {
+        } else if (arr[mid] > value) {
             high = mid - 1;
         } else {
             low = mid + 1;
@@ -20,15 +21,18 @@ function binarySearch(arr, n, value) {
 }
 
 // result
-console.log('二分查找非递归实现结果为：', binarySearch([1, 3, 4, 5, 6, 7, 8, 9, 9], 9, 5));
+console.log(
+    "二分查找非递归实现结果为：",
+    binarySearch([1, 3, 4, 5, 6, 7, 8, 9, 9], 9, 5)
+);
 
 // 递归实现
 function binarySearchRecursive(arr, low, high, value) {
-    if(low > high) return -1;
+    if (low > high) return -1;
     let mid = low + ((high - low) >> 1);
-    if(arr[mid] < value) {
+    if (arr[mid] < value) {
         return binarySearchRecursive(arr, mid + 1, high, value);
-    } else if(arr[mid] == value) {
+    } else if (arr[mid] == value) {
         return mid;
     } else {
         return binarySearchRecursive(arr, low, mid - 1, value);
@@ -36,21 +40,25 @@ function binarySearchRecursive(arr, low, high, value) {
 }
 
 // result
-console.log('二分查找递归实现结果为：', binarySearchRecursive([1, 3, 4, 5, 6, 7, 8, 9, 9], 0, 9, 5));
+console.log(
+    "二分查找递归实现结果为：",
+    binarySearchRecursive([1, 3, 4, 5, 6, 7, 8, 9, 9], 0, 9, 5)
+);
 
 // 二分查找变形问题
 // 1. 查找第一个值等于给定值
 function binarySearchVariant1(arr, n, value) {
-    let low = 0, high = n - 1;
-    while(low <= high) {
+    let low = 0,
+        high = n - 1;
+    while (low <= high) {
         let mid = low + ((high - low) >> 1);
-        if(arr[mid] == value) {
-            if(mid == 0 || arr[mid - 1] != value) {
+        if (arr[mid] == value) {
+            if (mid == 0 || arr[mid - 1] != value) {
                 return mid;
             } else {
                 high = mid - 1;
             }
-        } else if(arr[mid] > value) {
+        } else if (arr[mid] > value) {
             high = mid - 1;
         } else {
             low = mid + 1;
@@ -60,16 +68,17 @@ function binarySearchVariant1(arr, n, value) {
 
 // 2. 查找最后一个值等于给定值
 function binarySearchVariant2(arr, n, value) {
-    let low = 0, high = n - 1;
-    while(low <= high) {
+    let low = 0,
+        high = n - 1;
+    while (low <= high) {
         let mid = low + ((high - low) >> 1);
-        if(arr[mid] == value) {
-            if(mid == 0 || arr[mid + 1] != value) {
+        if (arr[mid] == value) {
+            if (mid == 0 || arr[mid + 1] != value) {
                 return mid;
             } else {
                 low = mid + 1;
             }
-        } else if(arr[mid] > value) {
+        } else if (arr[mid] > value) {
             high = mid - 1;
         } else {
             low = mid + 1;
@@ -79,11 +88,12 @@ function binarySearchVariant2(arr, n, value) {
 
 // 3. 查找第一个大于等于给定值
 function binarySearchVariant3(arr, n, value) {
-    let low = 0, high = n - 1;
-    while(low <= high) {
+    let low = 0,
+        high = n - 1;
+    while (low <= high) {
         let mid = low + ((high - low) >> 1);
-        if(arr[mid] >= value) {
-            if(mid == 0 || arr[mid - 1] < value) {
+        if (arr[mid] >= value) {
+            if (mid == 0 || arr[mid - 1] < value) {
                 return mid;
             } else {
                 high = mid - 1;
@@ -96,11 +106,12 @@ function binarySearchVariant3(arr, n, value) {
 
 // 4. 查找最后一个小于等于给定值
 function binarySearchVariant3(arr, n, value) {
-    let low = 0, high = n - 1;
-    while(low <= high) {
+    let low = 0,
+        high = n - 1;
+    while (low <= high) {
         let mid = low + ((high - low) >> 1);
-        if(arr[mid] <= value) {
-            if(mid == n - 1 || arr[mid + 1] > value) {
+        if (arr[mid] <= value) {
+            if (mid == n - 1 || arr[mid + 1] > value) {
                 return mid;
             } else {
                 low = mid + 1;
@@ -110,3 +121,90 @@ function binarySearchVariant3(arr, n, value) {
         }
     }
 }
+
+// 5. 有序旋转数组找出给定值 [4, 5, 6, 7, 1, 2, 3] 不重复
+function binarySearchRotate(arr, value) {
+    let low = 0,
+        high = arr.length - 1;
+    while (low <= high) {
+        let mid = low + ((high - low) >> 1);
+        if (arr[mid] == value) {
+            return mid;
+        }
+        if (arr[mid] > arr[low]) {
+            if (arr[mid] > value && arr[low] > value) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        } else {
+            if (arr[mid] < value && arr[high] < value) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+    }
+    return -1;
+}
+
+// result
+console.log(
+    "有序旋转数组结果为：",
+    binarySearchRotate([4, 5, 6, 7, 1, 2, 3], 0)
+);
+
+// code parse
+// low = 0, high = 6, mid = 3, arr[mid] = 7, 7 > 4 true, low = 3 + 1
+// low = 4, high = 6, mid = 5, arr[mid] = 2, 2 === 2 true, mid = 4,
+
+// 6. 旋转有序数组变形问题 找出旋转数组最小值 不重复
+function findBinarySearchMin(arr) {
+    let low = 0,
+        high = arr.length - 1;
+    while (low <= high) {
+        let mid = low + ((high - low) >> 1);
+        if (arr[mid + 1] < arr[mid]) {
+            return arr[mid + 1];
+        }
+        if (arr[mid - 1] > arr[mid]) {
+            return arr[mid];
+        }
+        if (arr[mid] > arr[low]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+// result
+console.log(
+    "旋转有序数组最小值结果为：",
+    findBinarySearchMin([4, 5, 6, 7, 2, 3])
+);
+
+// 7. 旋转有序数组变形问题 找出旋转数组最小值 存在重复值
+function findBinarySearchMinII(arr) {
+    let low = 0,
+        high = arr.length - 1;
+    while (low < high) {
+        let mid = low + ((high - low) >> 1);
+        if (arr[mid] > arr[high]) {
+            low = mid + 1;
+        } else if (arr[mid] < arr[high]) {
+            high = mid;
+        } else {
+            low = high - 1;
+        }
+    }
+    return arr[low];
+}
+
+// result
+console.log(
+    "旋转有序数组最小值结果为，存在重复值：",
+    findBinarySearchMinII([1, 3, 5]),
+    findBinarySearchMinII([2, 2, 2, 0, 1])
+);
---
+++ #12.js
@@ -0,0 +1,7 @@
+/**
+ * 散列表
+ * 散列表 + 链表 实现一个 LRU 缓存淘汰算法
+ */
+
+// waiting...
+// LRU(latest recently use) 最近最少使用
